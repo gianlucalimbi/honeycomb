dd2ae599fa05ab8e00af945b635c1a6db29c3c62
Gianluca Limbi <gianluca.limbi@gmail.com>
2017-09-01 23:12:34 +0200

    empty Service Provider

---
+++ src/HoneycombServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Honeycomb;
+
+use Illuminate\Support\ServiceProvider;
+
+class HoneycombServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
