1563146f61dd38e38931670234c20a62e6f27a82
Gianluca Limbi <gianluca.limbi@gmail.com>
2017-09-02 10:19:09 +0200

    added is_sequential_array and is_associative_array to helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,31 @@
 <?php
 
-//
+if (!function_exists('is_sequential_array')) {
+    /**
+     * Checks whether $var is a sequential array.
+     * E.g. [ <value>, <value>, <value>, ... ]
+     *
+     * @param $var
+     *
+     * @return boolean
+     */
+    function is_sequential_array($var)
+    {
+        return is_array($var) && array_merge($var) === $var && is_numeric(implode(array_keys($var)));
+    }
+}
+
+if (!function_exists('is_associative_array')) {
+    /**
+     * Checks whether $var is an associative array.
+     * E.g. [ <key> => <value>, <key> => <value>, <key> => <value>, ... ]
+     *
+     * @param $var
+     *
+     * @return boolean
+     */
+    function is_associative_array($var)
+    {
+        return is_array($var) && (array_merge($var) !== $var || !is_numeric(implode(array_keys($var))));
+    }
+}
