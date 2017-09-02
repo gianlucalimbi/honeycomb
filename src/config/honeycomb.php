f830a9723b1282e170d66369381987ecfaaf92f8
Gianluca Limbi <gianluca.limbi@gmail.com>
2017-09-02 12:01:41 +0200

    added basic configs

--- src/config/honeycomb.php
+++ src/config/honeycomb.php
@@ -2,6 +2,55 @@
 
 return [
 
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Camel Case Keys
+    |--------------------------------------------------------------------------
+    |
+    | When this flag is set to true, Honeycomb will convert all keys in the
+    | JSON response to be camelCase, otherwise the case of keys will be
+    | unchanged. Honeycomb custom keys are in snake_case.
+    |
+    */
+
+    'camel_case' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Feedback
+    |--------------------------------------------------------------------------
+    |
+    | By default, Honeycomb will use descriptive messages in the response
+    | using the Feedback class. By setting this flag to false, only the
+    | internal message will be used instead.
+    |
+    */
+
+    'use_feedback' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Per Page
+    |--------------------------------------------------------------------------
+    |
+    | Change the minimum and maximum allowed "per page" used in lists.
+    | If the values are incompatible, 'per_page_min' will be used.
+    |
+    */
+
+    'per_page_min' => 10,
+    'per_page_max' => 100,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Per Page Default
+    |--------------------------------------------------------------------------
+    |
+    | Change the default "per page" used in lists,
+    | if not supplied as a query argument.
+    |
+    */
+
+    'per_page_default' => 10,
 
 ];
