204efdf97eadba05a4b83fda449eac88205acd25
Gianluca Limbi <gianluca.limbi@gmail.com>
2017-09-02 10:45:37 +0200

    added basic error language lines

--- src/lang/en/errors.php
+++ src/lang/en/errors.php
@@ -2,6 +2,15 @@
 
 return [
 
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Api Errors Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'generic' => 'An error occurred. Please try again.',
+    'not_found' => 'The page you are trying to access does not exist.',
+    'authentication' => 'An error occurred. Please make sure you are logged in and try again.',
+    'validation' => 'An error occurred. Please check your data and try again.',
 
 ];
